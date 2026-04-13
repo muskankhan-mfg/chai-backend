3b31b3460571f4e3e7180871586971a64dc5f0a2
muskankhan-mfg <muskankhan95095@gmail.com>
2026-04-13 16:04:24 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
 
 const app = express();
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true   
+}))
+  
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded(endcoded({extended:true})));
 
+app.use(express.static("public"));
 
+app.use(cookieParser());
 export { app };
